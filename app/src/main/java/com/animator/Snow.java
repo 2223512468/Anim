6e7d75a84099a7a71e65f602bf32e75897e0c45d
2223512468 <2223512468@qq.com>
2018-03-10 14:37:36 +0800

    --

---
+++ app/src/main/java/com/animator/Snow.java
@@ -0,0 +1,30 @@
+package com.animator;
+
+import android.graphics.Bitmap;
+
+public class Snow {
+
+	Bitmap bitmap;
+
+	float x;
+
+	float y;
+
+	float speed;
+
+	float offset;
+
+	public Snow(Bitmap bitmap, float x, float y, float speed, float offset) {
+		this.bitmap = bitmap;
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.offset = offset;
+	}
+
+
+	
+	
+	
+
+}
